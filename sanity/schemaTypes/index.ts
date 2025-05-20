737770539cb59b0f4fa3bfe73fc080a540bd214b
yanko-dima <yanko.dmitriy@gmail.com>
2025-05-20 22:04:24 +0300

    create orderType and salesType

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -5,7 +5,17 @@ import { categoryType } from "./categoryType";
 import { postType } from "./postType";
 import { authorType } from "./authorType";
 import { productType } from "./productType";
+import { orderType } from "./orderType";
+import { salesType } from "@/sanity/schemaTypes/salesType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, productType],
+  types: [
+    blockContentType,
+    categoryType,
+    postType,
+    authorType,
+    productType,
+    orderType,
+    salesType,
+  ],
 };
